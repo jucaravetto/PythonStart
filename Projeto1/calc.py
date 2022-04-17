8af2ca905f1ef407785ba3c6eeb83128b3f6200f
Julianna Caravetto <julianna.caravetto@gmail.com>
2022-04-17 00:28:46 -0300

    Fazendo multiplicação e divisão

--- Projeto1/calc.py
+++ Projeto1/calc.py
@@ -3,11 +3,6 @@ print("Olá, vamos calcular!")
 number1 = float(input("Primeiro número: "))
 number2 = float(input("Segundo número: "))
 
-#print("Primeiro:")
-#print(number1)
-#print("Segundo:")
-#print(number2)
-
 soma=number1+number2
 print("\nA soma é: ", soma)
 
@@ -16,3 +11,12 @@ print("\nA subtração número 1 - número 2 é: ", subtracao1)
 
 subtracao2=number2-number1
 print("\nA subtração número 2 - número 1 é: ", subtracao2)
+
+mult=number1*number2
+print("\nA multiplicação é: ", mult)
+
+div1=number1/number2
+print("\nA divisão número 1 / número 2 é: ", div1)
+
+div2=number2/number1
+print("\nA divisão número 2 / número 1 é: ", div2)
