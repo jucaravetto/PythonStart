7d913178f45e78e6aaf46e654d50109d859ff593
Julianna Caravetto <julianna.caravetto@gmail.com>
2022-04-16 23:54:20 -0300

    Começando a calculadora

--- Projeto1/calc.py
+++ Projeto1/calc.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+print("Olá, vamos calcular!")
+
+number_1 = input("Primeiro número: ")
+number_2 = input("Segundo número: ")
+
+print("Primeiro:")
+print(number_1)
+print("Segundo:")
+print(number_2)
