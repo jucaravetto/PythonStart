a2c6634c42f9d5969a19dfec8d56f3e3f58044e7
Julianna Caravetto <julianna.caravetto@gmail.com>
2022-04-17 00:10:53 -0300

    Adicionando soma e subtração à calculadora

--- Projeto1/calc.py
+++ Projeto1/calc.py
@@ -1,9 +1,18 @@
 print("Olá, vamos calcular!")
 
-number_1 = input("Primeiro número: ")
-number_2 = input("Segundo número: ")
+number1 = float(input("Primeiro número: "))
+number2 = float(input("Segundo número: "))
 
-print("Primeiro:")
-print(number_1)
-print("Segundo:")
-print(number_2)
+#print("Primeiro:")
+#print(number1)
+#print("Segundo:")
+#print(number2)
+
+soma=number1+number2
+print("\nA soma é: ", soma)
+
+subtracao1=number1-number2
+print("\nA subtração número 1 - número 2 é: ", subtracao1)
+
+subtracao2=number2-number1
+print("\nA subtração número 2 - número 1 é: ", subtracao2)
